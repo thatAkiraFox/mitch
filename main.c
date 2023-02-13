a7c6a74a0bda76c56bb44bba5d8a94f2d4686664
Akira <49398464+thatAkiraFox@users.noreply.github.com>
2023-02-13 13:56:15 +0100

    Output-related changes/fixes
    * Separated the top and bottom padding `printf()` from the main output body to make everything more readable
    * Added a space before the newline character in the bottom padding `printf()` to fix some issues with certain oh-my-zsh themes

---
+++ main.c
@@ -0,0 +1,55 @@
+#include "headers.h"
+
+int main()
+{
+    // declare the struct containing the needed data
+    os_info_t os_info;
+
+    // initialize the struct
+    os_info.os_name = get_distro_name();
+    os_info.kernel_version = get_kernel_version();
+    os_info.username = get_username();
+    os_info.hostname = get_hostname();
+    os_info.shell = get_shell();
+
+    // top padding
+    printf("\n");
+
+    // formatted output
+    printf(BRED "%*s" RST "  %s  " BRED "%s\n" RST,
+        PADDING,
+        "User",
+        USER_ICON,
+        os_info.username);
+
+    printf(BGRN "%*s" RST "  %s  " BGRN "%s\n" RST,
+        PADDING,
+        "Host",
+        HOST_ICON,
+        os_info.hostname);
+
+    printf(BBLU "%*s" RST "  %s  " BBLU "%s\n" RST,
+        PADDING,
+        "Distro",
+        print_icon(os_info.os_name),
+        os_info.os_name);
+
+    printf(BYEL "%*s" RST "  %s  " BYEL "%s\n" RST,
+        PADDING,
+        "Kernel",
+        KERNEL_ICON,
+        os_info.kernel_version);
+
+    printf(BMAG "%*s" RST "  %s  " BMAG "%s\n" RST,
+        PADDING,
+        "Shell",
+        SHELL_ICON,
+        os_info.shell);
+
+    // bottom padding
+    printf(" \n");
+
+    free_all(&os_info);
+
+    return 0;
+}
